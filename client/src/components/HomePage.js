5640cc3ecddd203cb84965659192b642efc65be6
dlee46 <dannyl.dhl@gmail.com>
2018-07-16 16:59:49 -0400

    heroku

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://files.slack.com/files-pri/T0351JZQ0-FBQQ9R25N/8603567984_9382bb79ed_k.jpg" alt="" width={200} />
+            </div>
+        );
+    }
+}
+
+export default HomePage;
