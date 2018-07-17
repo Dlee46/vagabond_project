67899e839d1b92d844a16c6afa9357565f4d2799
dlee46 <dannyl.dhl@gmail.com>
2018-07-17 11:15:06 -0400

    added routes/controllers/references

---
+++ app/controllers/datab/posts_controller.rb
@@ -0,0 +1,36 @@
+class Datab::PostsController < ApplicationController
+
+    def index
+        @posts = Post.find(params[:city_id]).posts
+        render json: @posts
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        render json: @song
+    end
+
+    def create
+        @city = City.find(params[:city_id])
+        @post = @city.posts.create(post_params)
+        render json: @post
+    end
+
+    def update
+        @post Post.find(params[:id])
+        @post.update(post_params)
+        render json: @post
+    end
+
+    def destroy
+        @post = Post.find(params[:id]).destroy
+        render status: :ok
+    end
+
+private
+
+def post_params
+    params.require(:post).permit(:title, :description)
+end
+
+end
