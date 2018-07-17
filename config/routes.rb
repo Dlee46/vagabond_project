8258c976488f6d415ab3d80c87cd1d20d4f4706f
dlee46 <dannyl.dhl@gmail.com>
2018-07-17 14:24:23 -0400

    fixed city controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   namespace :datab do
-    resources :cities do
-      resources :posts
-    end
+      resources :cities do
+        resources :posts
+     end
   end
+
 
 end
