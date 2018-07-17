1df11a8099021c65ee10c2b29c68ad9e5043e04b
dlee46 <dannyl.dhl@gmail.com>
2018-07-17 15:59:25 -0400

    navbar placement

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import HomePage from './components/HomePage';
-import styled from 'styled-components'
 import CityHomePage from './components/CityHomePage';
 
-const NavBar = styled.div`
-    min-height: 10vh;
-    display: grid;
-    background-color: #009781;
-    grid-template-columns: 60vw 15vw 15vw;
-    justify-content: space-between;
-    padding: 1vh 0; 
-    
-    .siteName{
-        text-align: left;
-        margin-left: 10vw;
-        font-weight: bold;
-        align-self: center;
-        color:#f9f7ed;
-    }
-    border: 1px solid gray;
-    a {
-      font-size: larger;
-      align-self: center;
-      color:#f9f7ed;
-    }
-`
 
 
 class App extends Component {
   render() {
-    const NavBarComponent = (props) => (
-      <NavBar {...props} />
+    const HomePageWrapper = (props) => (
+      <HomePage {...props} />
     )
 
     const CityHomePageWrapper = (props) => {
@@ -43,7 +20,7 @@ class App extends Component {
       <Router>
         <div>
           <Switch>
-            <Route exact path='/' component={HomePage} />
+            <Route exact path='/' render={HomePageWrapper} />
             <Route exact path='/cities/:id' render={CityHomePageWrapper} />
           </Switch>
         </div>
