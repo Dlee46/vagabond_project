9740e4c5f42d215644627f24ce280bdff2447211
dlee46 <dannyl.dhl@gmail.com>
2018-07-17 09:24:32 -0400

    added styled to navbar and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import HomePage from './components/HomePage';
+import styled from 'styled-components'
+
+const NavBar = styled.div`
+
+    display: grid;
+    grid-template-columns: 60vw 15vw 15vw;
+    justify-content: space-between;
+    padding: 1vh 0;
+    .login{
+        margin-right: 5vw
+    }
+    .siteName{
+        text-align: left;
+        margin-left: 10vw
+        font-weight: 900;
+    }
+    border: 1px solid gray;
+`
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <h1>Travel Buddy</h1>
-          <Link to='/signup'>Sign Up</Link>
-          <Link to='/login'>Log In</Link>
+          <NavBar>
+            <div className="siteName">
+              Travel Buddy
+            </div>
+            <Link to='/signup'>Sign Up</Link>
+            <Link className=".login" to='/login'>Log In</Link>
+          </NavBar>
           <Switch>
             <Route exact path='/' component={HomePage} />
           </Switch>
