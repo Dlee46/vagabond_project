0f811009ea3f14ce2056084db9a7938dc0b8fbbc
dlee46 <dannyl.dhl@gmail.com>
2018-07-19 10:31:11 -0400

    fixed links on HomePage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components'
 import { Card, Image } from 'semantic-ui-react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 
 const SplashImage = styled.div`
@@ -37,6 +38,48 @@ const Cardbox = styled.div`
 `
 
 class HomePage extends Component {
+    state = {
+        cities: [],
+        atlantaId: "",
+        londonId: "",
+        sanFranId: ""
+    }
+
+    componentDidMount = async () => {
+        await this.fetchCityNamesAndId()
+    }
+
+    fetchCityNamesAndId = () => {
+        let atlantaId = 0
+        let londonId = 0
+        let sanFranId = 0
+        axios.get('/datab/cities/')
+            .then((response) => {
+                console.log(response.data)
+                return this.setState({ cities: response.data })
+            })
+            .then(() => {
+                let atlanta = this.state.cities.filter((city) => {
+                    // just in case people ever accidentally make lower case name for city
+                    return city.name.toUpperCase() === "ATLANTA"
+                })
+                let london = this.state.cities.filter((city) => {
+                    // just in case people ever accidentally make lower case name for city
+                    return city.name.toUpperCase() === "LONDON"
+                })
+                let sanFran = this.state.cities.filter((city) => {
+                    // just in case people ever accidentally make lower case name for city
+                    return city.name.toUpperCase() === "SAN FRANCISCO"
+                })
+                atlantaId = `/cities/${atlanta[0].id}`
+                londonId = `/cities/${london[0].id}`
+                sanFranId = `/cities/${sanFran[0].id}`
+                this.setState({ atlantaId, londonId, sanFranId })
+                console.log(atlantaId)
+            })
+    }
+
+
     render() {
         return (
             <div>
@@ -47,7 +90,7 @@ class HomePage extends Component {
                 <Content>
                     <Cardbox>
                         <Card>
-                            <a href="/cities/1" alt="">
+                            <a href={this.state.atlantaId} alt="">
                                 <Image src="https://secondcropcreative.com/perfectloops/images/chicago-timelapse.gif" height={200} />
                                 Atlanta
                             </a>
@@ -58,7 +101,7 @@ class HomePage extends Component {
 
                     <Cardbox>
                         <Card>
-                            <a href="" alt="">
+                            <a href={this.state.londonId} alt="">
                                 <Image src=" https://static.wixstatic.com/media/92dd1d_f6e95de6a63c4021ba96391bcdf27a15~mv2_d_2560_1338_s_2.gif" height={200} />
                                 London
                             </a>
@@ -67,7 +110,7 @@ class HomePage extends Component {
 
                     <Cardbox>
                         <Card>
-                            <a href="" alt="">
+                            <a href={this.state.sanFranId} alt="">
                                 <Image src="https://i.pinimg.com/originals/3c/76/ef/3c76ef5230967fe6ef22c07e126f4697.gif" height={200} />
                                 San Francisco
                             </a>
